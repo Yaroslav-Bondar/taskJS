3780a19b6786f877c7da28e58e048ae70ceebe6a
Yaroslav-Bondar <jamnetbox@gmail.com>
2021-06-18 19:13:41 +0300

    decided calculateFactorial.js

---
+++ tasks_from_learn.javascript.ru/recursion/calculateFactorial.js
@@ -0,0 +1,14 @@
+// https://learn.javascript.ru/task/factorial
+
+function factorial (n) {
+    if(n == 1) {
+        n = 1
+        
+    }
+    else {
+        n = n * factorial(n - 1)
+    }
+    console.log(n)
+    return n
+}
+factorial(3)
