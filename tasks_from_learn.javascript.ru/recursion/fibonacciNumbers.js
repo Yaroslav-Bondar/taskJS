42c6e02131f9d6eefa036b6db3119d74b103b74d
Yaroslav-Bondar <jamnetbox@gmail.com>
2021-06-26 18:50:18 +0300

    decided fibonacciNumbers.js

---
+++ tasks_from_learn.javascript.ru/recursion/fibonacciNumbers.js
@@ -0,0 +1,29 @@
+// https://learn.javascript.ru/task/fibonacci-numbers
+
+let tmp1 = 1
+let tmp2 = 1
+let res
+
+// loop
+function fib(n) {
+    for (let i = 2; i < n; i++) {
+            res = tmp1 + tmp2
+            tmp1 = tmp2
+            tmp2 = res
+    }
+    return res
+}
+// console.log(fib(77))
+// console.log(fib(3))
+
+// recursion
+function fib(n) {
+    if (n == 2) return
+    res = tmp1 + tmp2
+    tmp1 = tmp2
+    tmp2 = res
+    fib(n - 1)
+    return res
+    
+}
+console.log(fib(77))
