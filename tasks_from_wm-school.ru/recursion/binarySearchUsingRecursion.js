45bd053001d87701eb1046a88f79396fceed73c5
Yaroslav-Bondar <jamnetbox@gmail.com>
2021-07-03 19:00:34 +0300

    decided mergeSortRecursion.js

--- tasks_from_wm-school.ru/recursion/binarySearchUsingRecursion.js
+++ tasks_from_wm-school.ru/recursion/binarySearchUsingRecursion.js
@@ -3,30 +3,66 @@
 
 // Напишите программу на JavaScript для двоичного поиска.
 
-// Двоичный (бинарный) поиск (также известен как метод деления пополам) — классический алгоритм поиска элемента в отсортированном массиве (векторе), использующий дробление массива на половины. Используется в информатике, вычислительной математике и математическом программировании.
-
+// Двоичный (бинарный) поиск (также известен как метод деления пополам) — классический алгоритм поиска
+// элемента в отсортированном массиве (векторе), использующий дробление массива на половины. 
+// Используется в информатике, вычислительной математике и математическом программировании.
 // Пример массива: [0,1,2,3,4,5,6]
 // Ожидаемый результат: document.writeln(l.br_search (5)) вернет «5»
-let array = [0,1,2,3,4,5,6]
+let array = [0,2,3,4,5,6,7,8,9,11,20,30,35,40,45,50]
 // console.log(Math.round(array.length / 2 - 1))
-function bs(arr, s) {
-    let midIndex = Math.round(arr.length / 2 - 1)
-    let leftIndex, rightIndex, f
-    
-    // let len_2 = arr.length / 2 
-    while(arr[midIndex] != s) {
-        if(arr[midIndex] < s) {
-            leftIndex = midIndex
-            rightIndex = arr.length - 1
-            midIndex = Math.round(rightIndex - leftIndex / 2)
-        }
-        else if(arr[midIndex] > s) {
-            leftIndex = 0
-            rightIndex = midIndex
-            midIndex = Math.round(rightIndex - leftIndex / 2)            
-        }
-        if (arr[midIndex] === s) f = arr[midIndex]
-    }
-    return f
+// console.log(Math.round(4 / 2 - 1))
+// console.log(array[12])
+let longArr = new Array()
+for (let i = 0; i < 200; i++) {
+    longArr.push(i)
 }
-console.log(bs(array,4))
+function bs(arr, s) {
+    let mInd = Math.round(arr.length / 2 - 1)
+    let lInd = 0 
+    let rInd = arr.length - 1
+    while(1) {
+        if(arr[mInd] < s) {
+            lInd = mInd
+            mInd = Math.ceil((rInd + lInd) / 2)
+        }
+        else if(arr[mInd] > s) {
+            rInd = mInd
+            mInd = Math.floor((rInd + lInd) / 2)            
+        }
+        if (arr[mInd] === s) {
+            console.log('Index ', mInd, ' Element ', arr[mInd])
+            break
+        }
+        if (mInd === lInd || mInd === rInd && arr[mInd] !== s) {
+            console.log('Element not found')
+            break
+        } 
+    }
+}
+// bs(array, 3)
+// console.log(bs(longArr,200))
+
+// recursion
+let mInd = Math.round(array.length / 2 - 1)
+let lInd = 0 
+let rInd = array.length - 1
+function bs(arr, s) {
+    if(arr[mInd] < s) {
+        lInd = mInd
+        mInd = Math.ceil((rInd + lInd) / 2)
+    }
+    else if(arr[mInd] > s) {
+        rInd = mInd
+        mInd = Math.floor((rInd + lInd) / 2)            
+    }
+    if (arr[mInd] === s) {
+        console.log('Index ', mInd, ' Element ', arr[mInd])
+        return
+    }
+    if (mInd === lInd || mInd === rInd && arr[mInd] !== s) {
+        console.log('Element not found')
+        return
+    }
+    bs(arr, s) 
+}
+bs(array, 35)
