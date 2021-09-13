f6ab4a42e150e6b05c7d16d7e539050d31bbd9bb
Yaroslav-Bondar <jamnetbox@gmail.com>
2021-09-13 18:02:33 +0300

    modified tasks for promises

--- tasks_from_learn.javascript.ru/promise/delayOnPromise.js
+++ tasks_from_learn.javascript.ru/promise/delayOnPromise.js
@@ -4,10 +4,20 @@
 // Функция delay(ms) должна возвращать промис, который перейдёт в 
 // состояние «выполнен» через ms миллисекунд, так чтобы мы могли добавить к нему .then:
 
+// function delay(ms) {
+//   return new Promise(function(resolve, reject) {
+//     setTimeout(resolve, ms)
+//   })
+// }
+
+// delay(3000).then(() => console.log(`выполнилось через 3 секунды`));
+
+// modified
+
 function delay(ms) {
-  return new Promise(function(resolve, reject) {
-    setTimeout(resolve, ms)
+  return new Promise((resolve)=> {
+  	setTimeout(()=>resolve(ms), ms)
   })
 }
 
-delay(3000).then(() => console.log(`выполнилось через 3 секунды`));
+delay(4000).then((ms) => console.log(`выполнилось через ${ms / 1000} секунды`));
