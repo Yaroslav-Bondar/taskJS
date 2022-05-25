ca82fa7998833211a57384a59e47eaa6d655cac0
Yaroslav-Bondar <jamnetbox@gmail.com>
2022-05-25 08:59:52 +0300

    upgrade task-1 by Pavel Tarasov

--- tasks_from_interviews/pavel-tarasov/task-1/task-1.js
+++ tasks_from_interviews/pavel-tarasov/task-1/task-1.js
@@ -11,11 +11,15 @@ https://youtu.be/GYaZLJQIfrs
 // my solution
 function replaceItems(arr, item, replaceItem) {
     const reg = new RegExp(`${item}`, 'g');
-    return arr.join('').replace(reg, `${replaceItem}`).split(''); 
+    // replace
+    const stringArr = arr.join('').replace(reg, `${replaceItem}`).split('');
+    // convert string to number
+    const res = stringArr.map(item => +item ? +item : item);
+    return  res;
 }
-
+// solution from interview
 function replaceItems2(arr, item, replaceItem) {
     return arr.map(el => el === item ? replaceItem : el);
 }
-// console.log(replaceItems([1,2,3,4,2], 2, 'a')); //  [1,'a',3,4,'a']
-console.log(replaceItems2([1,2,3,4,2], 2, 'a')); //  [1,'a',3,4,'a']
+console.log(replaceItems([1,2,3,4,2], 2, 'a')); //  [1,'a',3,4,'a']
+// console.log(replaceItems2([1,2,3,4,2], 2, 'a')); //  [1,'a',3,4,'a']
