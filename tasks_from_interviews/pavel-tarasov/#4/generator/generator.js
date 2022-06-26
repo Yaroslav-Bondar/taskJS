661ce1f0e15ab5e7cf2d6f9ead05a30b17b17146
Yaroslav-Bondar <jamnetbox@gmail.com>
2022-06-26 07:25:54 +0300

    task #4 generator

--- tasks_from_interviews/pavel-tarasov/#4/generator/generator.js
+++ tasks_from_interviews/pavel-tarasov/#4/generator/generator.js
@@ -1,12 +1,16 @@
-function sequence(val, step) {
+function sequence(val = 0, step) {
     let gen = 0;
-    return function() {
-        if(gen) {
-            if(step) {
-                return gen + step
-            }
-        }
-        return gen ? gen +  
-        return gen
-    }
+    return () => !gen ? gen += val : gen && !step ? gen += 1 : gen && step ? gen += step : true; 
 }
+
+const generator = sequence(10, 3);
+const generator1 = sequence(11);
+const generator2 = sequence(7, 1);
+
+console.log(generator());
+console.log(generator());
+console.log(generator2());
+console.log(generator());
+console.log(generator1());
+console.log(generator1());
+console.log(generator2());
